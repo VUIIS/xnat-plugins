8bff73635a95d0d37563e4883ab6f3966828d079
Brian D. Boyd <bud42@users.noreply.github.com>
2022-05-09 15:20:26 -0400

    Update subjGenProcDataXnatPlugin.java

--- vuiis-datatype-subjGenProcData/src/main/java/subjGenProcDataXnatPlugin.java
+++ vuiis-datatype-subjGenProcData/src/main/java/subjGenProcDataXnatPlugin.java
@@ -4,6 +4,6 @@ import org.nrg.framework.annotations.XnatDataModel;
 
 @XnatPlugin(value = "subjGenProcDataXnatPlugin", name = "DAX subjGenProcData XNAT Plugin",
             description = "This is the DAX subjGenProcData data type for XNAT Plugin.",
-            dataModels = {@XnatDataModel(value = "proc:subjGenProcData", singular = "Subject Processing", plural="Subject Processings", code = "Subj Proc")})
+            dataModels = {@XnatDataModel(value = "proc:subjGenProcData", singular = "SubjectProcessing", plural="SubjectProcessings", code = "SubjProc")})
 public class subjGenProcDataXnatPlugin {
 }
